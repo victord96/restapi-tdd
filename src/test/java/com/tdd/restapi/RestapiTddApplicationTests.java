9b3918752e48aebc523773e37db505986f3657ce
Victor <victor1996d@gmail.com>
2022-07-16 13:17:36 +0200

    initialised project

---
+++ src/test/java/com/tdd/restapi/RestapiTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
